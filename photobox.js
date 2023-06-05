d9f91b4e6a703ba675b3c1c51791cd922edf6230
Stanislas JEANGEY <jeangey1u@etu.univ-lorraine.fr>
2023-06-05 19:27:38 +0200

    chargement des images dans l'index2.html

---
+++ photobox.js
@@ -0,0 +1,13 @@
+import * as gallerie from './lib/gallery.js';
+import * as gallerie_ui from './lib/gallery_ui.js';
+import {display_gallerie} from "./lib/gallery_ui.js";
+
+const getGallerie = function () {
+    let pr = gallerie.load()
+
+    pr.then(function (data) {
+        gallerie_ui.display_gallerie(data)
+    })
+};
+
+getGallerie();
