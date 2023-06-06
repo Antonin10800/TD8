98c6bd6bb48d649c914108098d45144ef48af0d6
Stanislas JEANGEY <jeangey1u@etu.univ-lorraine.fr>
2023-06-06 10:27:12 +0200

    event

--- photobox.js
+++ photobox.js
@@ -1,5 +1,7 @@
 import * as gallerie from './lib/gallery.js';
 import * as gallerie_ui from './lib/gallery_ui.js';
+import * as photoloader from "./lib/photoloader.js";
+import * as photo_ui from "./lib/photo_ui.js";
 
 const getGallerie = function () {
     let pr = gallerie.load()
@@ -9,7 +11,41 @@ const getGallerie = function () {
     })
 };
 
+
+const getPicture = function (idPicture) {
+    let pr = photoloader.loadPicture(idPicture);
+
+    pr.then(function (response) {
+        photo_ui.displayPicture(response.photo);
+
+        getResource(response);
+        getComments(response);
+
+    });
+    pr.catch(function (error) {
+        console.log("Error getPicture" + error);
+    });
+}
+
+function getResource(dataImg) {
+    let parsedLink = photo_ui.parsLink();
+
+    let pr = photoloader.loadResource(dataImg.links.categorie.href, parsedLink);
+    pr.then(function (response) {
+            photo_ui.displayRessource(response);
+        }
+    );
+}
+
+function getComments(dataImg){
+    let pasrsedLink = photo_ui.parsLink();
+    let pr = photoloader.loadComments(dataImg.links.comments.href, pasrsedLink);
+    pr.then(function(data){
+        photo_ui.displayComments(data);
+    });
+
+}
+
 const load = document.getElementById('load_gallery');
 load.addEventListener('click', getGallerie);
 
-//getGallerie();
